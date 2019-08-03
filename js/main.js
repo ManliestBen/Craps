8475691ff837f74050a3ff2970f3030c48c59bb5
ManliestBen <benjamintmanley@gmail.com>
2019-08-02 20:29:25 -0500

    Added chips to html

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 
 /*-------State Variables-------*/
-
+let chipTotal, betTotal;
 
 
 /*-------Cached Element References-------*/
@@ -17,6 +17,14 @@ document.querySelector('main').addEventListener('click', handleClick);
 
 
 /*-------Functions-------*/
+init();
+
+function init() {
+    chipTotal = 500;
+    betTotal = 0;
+}
+
 function handleClick(evt) {
     console.log(evt.target);
 }
+
