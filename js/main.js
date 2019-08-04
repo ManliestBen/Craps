5b1afc3b7b69192fdf322292107f13810b9192d1
ManliestBen <benjamintmanley@gmail.com>
2019-08-04 15:46:42 -0500

    Payout works for point and side bet fields

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
+/*-------State Variables-------*/
+let chipTotal, betTotal, point, die1Num, die1Text, die2Num, die2Text;
+let payout = 0;
+
+
 /*-------Constants-------*/
 const sideObj = {
-    anySeven: {multiplier:4, currentBet:0, winnerIf:7},
-    hardFour: {multiplier:7, currentBet:0, winnerIf:4},
-    hardTen: {multiplier:7, currentBet:0, winnerIf:10},
-    hardSix: {multiplier:9, currentBet:0, winnerIf:6},
-    hardEight: {multiplier:9, currentBet:0, winnerIf:8},
-    anyThree: {multiplier:15, currentBet:0, winnerIf:3},
-    anyEleven: {multiplier:15, currentBet:0, winnerIf:11},
-    anyTwo: {multiplier:30, currentBet:0, winnerIf:2},
-    anyTwelve: {multiplier:30, currentBet:0, winnerIf:12},
-    anyCraps: {multiplier:7, currentBet:0, winnerIf:2/3/12}
+    anySeven: {multiplier:4, currentBet:0, winnerIf:7, dieSpecific: 0},
+    hardFour: {multiplier:7, currentBet:0, winnerIf:4, dieSpecific: 1},
+    hardTen: {multiplier:7, currentBet:0, winnerIf:10, dieSpecific: 1},
+    hardSix: {multiplier:9, currentBet:0, winnerIf:6, dieSpecific: 1},
+    hardEight: {multiplier:9, currentBet:0, winnerIf:8, dieSpecific: 1},
+    anyThree: {multiplier:15, currentBet:0, winnerIf:3, dieSpecific: 0},
+    anyEleven: {multiplier:15, currentBet:0, winnerIf:11, dieSpecific: 0},
+    anyTwo: {multiplier:30, currentBet:0, winnerIf:2, dieSpecific: 0},
+    anyTwelve: {multiplier:30, currentBet:0, winnerIf:12, dieSpecific: 0},
+    anyCraps: {multiplier:7, currentBet:0, winnerIf:2/3/12, dieSpecific: 0}
 }
 
 const centerObj = {
-    passLine: {multiplier:1, currentBet:0, winnerIf:1},
-    dontPass: {multiplier:1, currentBet:0, winnerIf:1},
-    fieldBottom: {multiplier:1, currentBet:0, winnerIf:1},
-    come: {multiplier:1, currentBet:0, winnerIf:1}
+    passLine: {multiplier:1, currentBet:0, winnerIf:1, isPointActive:0},
+    dontPass: {multiplier:1, currentBet:0, winnerIf:1, isPointActive:0},
+    fieldBottom: {multiplier:1, currentBet:0, winnerIf:1, isPointActive:0},
+    come: {multiplier:1, currentBet:0, winnerIf:1, isPointActive:0}
 }
 
 const pointObj = {
@@ -29,9 +34,6 @@ const pointObj = {
     point10: {multiplier:(9/5), currentBet:0, winnerIf:10}
 }
 
-
-/*-------State Variables-------*/
-let chipTotal, betTotal, payout, die1Num, die1Text, die2Num, die2Text;
 
 
 /*-------Cached Element References-------*/
@@ -56,7 +58,7 @@ function reset(){
 }
 function init() {
     
-    chipTotal = 500;
+    chipTotal = 1500;
     betTotal = 0;
     dimChips();
     render();
@@ -111,6 +113,7 @@ function rollDice(){
     die1Text = "&#x268" + (die1Num - 1) + " ";
     die2Text = "&#x268" + (die2Num - 1) + " ";
     document.getElementById('rolledDice').innerHTML = die1Text + die2Text;
+    payBets();
 }
 
 function render(){
@@ -127,4 +130,25 @@ function dimChips(){
     document.getElementById('chip25').style.visibility= ((chipTotal < 25) ? 'hidden' : 'visible');
     document.getElementById('chip5').style.visibility= ((chipTotal < 5) ? 'hidden' : 'visible');
     document.getElementById('chip1').style.visibility= ((chipTotal < 1) ? 'hidden' : 'visible');
+}
+
+function payBets(){
+    for (bet in pointObj){
+        if (pointObj[bet].currentBet > 0 && ((die1Num + die2Num) === pointObj[bet].winnerIf)) {
+            payout = payout + parseInt(pointObj[bet].multiplier * pointObj[bet].currentBet);
+        };
+    }
+    for (bet in sideObj){
+        if (sideObj[bet].currentBet > 0 && (sideObj[bet].winnerIf === (die1Num + die2Num)) && sideObj[bet].dieSpecific === 0) {
+            payout = payout + parseInt(sideObj[bet].multiplier * sideObj[bet].currentBet);
+        };
+    }
+    for (bet in sideObj){
+        if (sideObj[bet].currentBet > 0 && (sideObj[bet].winnerIf === (die1Num + die2Num)) && (die1Num === die2Num) && sideObj[bet].dieSpecific === 1) {
+            payout = payout + parseInt(sideObj[bet].multiplier * sideObj[bet].currentBet);
+        };
+    }
+    chipTotal += payout;
+    payout = 0;
+    render();
 }
