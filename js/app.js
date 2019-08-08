1a33a699b045cd9a3d49c68549299e9044e1abba
ManliestBen <benjamintmanley@gmail.com>
2019-08-07 23:05:05 -0500

    Fixed sound overlap

--- js/app.js
+++ js/app.js
@@ -214,7 +214,7 @@ function payBetsNoPoint(){
         
         callDice();
         if (payout > 0) {
-            setTimeout(function(){audioYay.play();},200);
+            setTimeout(function(){audioYay.play();},1000);
             confetti.start(payout);
         }
     
@@ -300,7 +300,7 @@ function payBets(){  // Pay out bets for when a point is active
    
     callDice();
     if (payout > 0) {
-        setTimeout(function(){audioYay.play();},200);
+        setTimeout(function(){audioYay.play();},1000);
         confetti.start(payout);
     }
     chipTotal += payout;
